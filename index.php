2db6026e1f64fb5a591846072403215642d2daaf
myname <myname@local>
2020-04-18 18:53:56 +0300

    was added some new files

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<html></html>
+<html>
+	<title></title>
+	<head></head>
+	<body>
+		<h1>Page</h1>
+	</body>
+</html>
+<?php
+
+echo '123';
+echo '--123';
+
+?>
---
+++ index2.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <a href=""></a>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="Content-Language" content="ru-RU" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="/assets/12cbfc30/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="/assets/8f03ca51/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="/assets/8f03ca51/font-awesome/css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="/assets/8f03ca51/css/bootstrap-yii.css" />
+<link rel="stylesheet" type="text/css" href="/assets/8f03ca51/css/jquery-ui-bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="/assets/12cbfc30/css/main.css" />
+<link rel="stylesheet" type="text/css" href="/assets/12cbfc30/css/flags.css" />
+<link rel="stylesheet" type="text/css" href="/assets/12cbfc30/css/yupe.css" />
+<link rel="stylesheet" type="text/css" href="src/index.css" />
+<script type="text/javascript" src="/assets/808cad9d/jquery.js"></script>
+<script type="text/javascript" src="/assets/8f03ca51/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/assets/8f03ca51/js/bootstrap-noconflict.js"></script>
+<script type="text/javascript" src="/assets/8f03ca51/bootbox/bootbox.min.js"></script>
+<script type="text/javascript" src="/assets/8f03ca51/notify/notify.min.js"></script>
+<script type="text/javascript" src="/assets/808cad9d/jquery.ba-bbq.js"></script>
+<script type="text/javascript" src="/assets/12cbfc30/js/blog.js"></script>
+<script type="text/javascript" src="/assets/12cbfc30/js/bootstrap-notify.js"></script>
+<script type="text/javascript" src="/assets/12cbfc30/js/jquery.li-translit.js"></script>
+<script type="text/javascript">
+    /*<![CDATA[*/
+    var baseUrl = 'http://194.67.90.41';
+    /*]]>*/
+    </script>
+    <title>Записи</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <script type="text/javascript">
+    var yupeTokenName = 'YUPE_TOKEN';
+    var yupeToken = 'X2V4VXFTY0E0ZXdIa3N3V1l6elNZVDh4YVpPVk5nRjQlmEL6koaLmJ2_SHSSFbUob7xF6HfkjXfIXcc0GGwI4Q==';
+    </script>
+    <!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <link rel="stylesheet" href="http://yandex.st/highlightjs/8.2/styles/github.min.css">
+    <script src="http://yastatic.net/highlightjs/8.2/highlight.min.js"></script>
+</head>
+<?php
+echo '123';
+?>
+
+<body>
+    <div class="fls" style="color:red;"></div>
+    <div class="block"></div>
+    <nav style="background-color:green;" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header"><button class="navbar-toggle btn btn-default" data-toggle="collapse" data-target="#yii_booster_collapse_yw2" id="yw3" name="yt0" type="button"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a href="/" class="navbar-brand"><img width="38" height="38" title="Yupe!" src="/assets/12cbfc30/images/logo.png" alt="Yupe!" /></a></div>
+            <div class="collapse navbar-collapse" id="yii_booster_collapse_yw2">
+                <ul id="yw4" class="nav navbar-nav">
+                    <li class="listItem active"><a class="listItemLink" title="Главная страница сайта" href="/">Главная</a></li>
+                    <li class="listItem"><a class="listItemLink" title="Магазин" href="/store/product/index">Магазин</a></li>
+                    <li class="listItem submenuItem dropdown">
+                        <a class="listItemLink dropdown-toggle" title="Блоги" data-toggle="dropdown" href="/blogs">Блоги <span class="caret"></span></a>
+                        <ul id="yw5" class="dropdown-menu">
+                            <li class="listItem">
+                                <a class="listItemLink" title="Пользователи" tabindex="-1" href="/users">Пользователи</a></li>
+                        </ul>
+                    </li>
+                    <li class="listItem">
+                        <a class="listItemLink" title="FAQ" href="/feedback/contact/faq">FAQ</a></li>
+                    <li class="listItem">
+                        <a class="listItemLink" title="Контакты" href="/feedback/contact/index">Контакты</a></li>
+                    <li class="listItem login-text">
+                        <a class="listItemLink" title="Регистрация на сайте" href="/registration">Регистрация</a></li>
+                    <li class="listItem login-text">
+                        <a class="listItemLink" title="Войти на сайт" href="/login">Войти</a></li>
+                </ul>
+                <ul class="pull-right nav navbar-nav" id="yw6">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class='iconflags iconflags-ru'></i>
+                            ru <span class="caret"></span></a>
+                        <ul style="min-width: 20px;" id="yw7" class="dropdown-menu">
+                            <li>
+                                <a tabindex="-1" href="/uk/"><i class="iconflags iconflags-uk"></i> uk</a></li>
+                            <li>
+                                <a tabindex="-1" href="/en/"><i class="iconflags iconflags-en"></i> en</a></li>
+                            <li>
+                                <a tabindex="-1" href="/zh/"><i class="iconflags iconflags-zh"></i> zh</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav><!-- container -->
+    <div class='container'>
+        <!-- flashMessages -->
+        <div id="yw8"></div> <!-- breadcrumbs -->
+        <ul class="breadcrumb">
+            <li><a href="/">Главная</a></li>
+            <li class="active">Записи</li>
+        </ul>
+        <div class="row">
+            <!-- content -->
+            <section class="col-sm-9 content">
+                <div id="yw0" class="list-view">
+                    <div class="items">
+                        <span class="empty">Нет результатов.</span></div>
+                    <div class="keys" style="display:none" title="/"></div>
+                </div>
+            </section>
+            <!-- content end-->
+            <!-- sidebar -->
+            <aside class="col-sm-3 sidebar">
+                <p>
+                    <a class="btn btn-success" style="width: 100%;" href="/post/write"><i class='glyphicon glyphicon-pencil'></i> Добавить запись</a> </p>
+                <div class="widget blogs-widget">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Интересно</h3>
+                        </div>
+                        <div class="panel-body">
+                            <a title="Авторские вебинары по Yii2 !" target="_blank" href="http://www.elisdn.ru/oop-week?utm_medium=affiliate&amp;utm_source=yupe"><img style="width: 100%" src="/assets/12cbfc30/images/elisdn.png" alt="Авторские вебинары по Yii2 !" /></a>
+                            <hr />
+                            <a title="amylabs - разработка на Юпи! и Yii !" target="_blank" href="http://amylabs.ru?from=yupe-rb"><img style="width: 100%" src="/assets/12cbfc30/images/amylabs.png" alt="amylabs - разработка на Юпи! и Yii !" /></a> </div>
+                    </div>
+                </div>
+                <div class="widget">
+                    <a href="https://www.jetbrains.com/phpstorm/" target="_blank"><img src="/assets/12cbfc30/images/logo_phpstorm.png" alt="" /></a>
+                </div>
+                <div class="widget stream-widget">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><i class="glyphicon glyphicon-pencil"></i>
+                            <h3 class="panel-title" style="display: inline;">Обсуждают</h3>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="panel-body" id="yw2">
+                            <ul class="list-unstyled">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="widget last-posts-widget">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title" style="display: inline;">Последние записи</h3>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="panel-body" id="yw3">
+                            <ul class="list-unstyled">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="widget blogs-widget">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><i class="glyphicon glyphicon-pencil"></i>
+                            <h3 class="panel-title" style="display: inline;">Блоги</h3>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="panel-body" id="yw4">
+                            <ul class="list-unstyled">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="widget tags-cloud-widget">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title" style="display: inline;">Теги</h3>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="panel-body" id="yw5">
+                        </div>
+                    </div>
+                </div>
+            </aside>
+            <!-- sidebar end -->
+        </div>
+        <!-- footer -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="col-sm-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="https://twitter.com/yupecms" target="_blank">Твиттер</a>
+                        </li>
+                        <li><a href="http://vk.com/amylabs" target="_blank">ВКонтакте</a></li>
+                        <li><a href="https://www.facebook.com/amylabs.ru" target="_blank">Фейсбук</a></li>
+                        <li><a href="https://plus.google.com/+AmylabsRu" target="_blank">Google+</a></li>
+                        <li><a href="https://yupe.ru/blogs/yupe-mini-cms-yii" target="_blank">Блог</a></li>
+                        <li><a href="https://yupe.ru/" target="_blank">Сайт</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="https://yupe.ru/docs/index.html" target="_blank">Документация</a></li>
+                        <li><a href="http://api.yupe.ru/" target="_blank">API</a></li>
+                        <li><a href="http://talk.yupe.ru/" target="_blank">Форум</a></li>
+                        <li><a href="https://yupe.ru/faq/" target="_blank">Вопросы и ответы</a></li>
+                        <li><a href="https://yupe.ru/contacts?from=footer" target="_blank">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="https://yupe.ru/about" target="_blank">О проекте</a></li>
+                        <li><a href="https://yupe.ru/docs/yupe/team.html" target="_blank">Команда</a></li>
+                        <li><a href="https://github.com/yupe/yupe/" target="_blank">GitHub</a>
+                        </li>
+                        <li><a href="https://github.com/yupe/yupe-ext/" target="_blank">Доп. модули</a></li>
+                        <li><a href="https://yupe.ru/docs/yupe/capability.html" target="_blank">Возможности</a></li>
+                        <li><a href="https://yupe.ru/docs/yupe/assistance.project.html" target="_blank">Помощь проекту</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="http://allframeworks.ru" target="_blank">allframeworks</a>
+                        </li>
+                        <li><a href="http://amylabs.ru" target="_blank">amylabs</a></li>
+                        <li><a href="https://yupe.ru/service/support?from=yupe" target="_blank">Коммерческая поддержка</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="nav nav-pills nav-stacked">
+                    <li>© 2012 - 2020</li>
+                </ul>
+            </div>
+        </div>
+        <!-- footer end -->
+    </div>
+    <div class='notifications top-right' id="notifications"></div>
+    <!-- container end -->
+    <script type="text/javascript" src="/assets/8f4619dd/listview/jquery.yiilistview.js"></script>
+    <script type="text/javascript">
+    /*<![CDATA[*/
+    jQuery(function($) {
+        (function($) {
+            $('<div>').appendTo('body').load('/debug/5e971e67d0882/toolbar', function() {
+                if (window.localStorage && localStorage.getItem('yii2-debug-toolbar') == 'minimized') {
+                    $('#yii2-debug-toolbar').hide();
+                    $('#yii2-debug-toolbar-min').show();
+                } else {
+                    $('#yii2-debug-toolbar-min').hide();
+                    $('#yii2-debug-toolbar').show();
+                }
+                $('#yii2-debug-toolbar .yii2-debug-toolbar-toggler').click(function() {
+                    $('#yii2-debug-toolbar').hide();
+                    $('#yii2-debug-toolbar-min').show();
+                    if (window.localStorage) {
+                        localStorage.setItem('yii2-debug-toolbar', 'minimized');
+                    }
+                });
+                $('#yii2-debug-toolbar-min .yii2-debug-toolbar-toggler').click(function() {
+                    $('#yii2-debug-toolbar-min').hide();
+                    $('#yii2-debug-toolbar').show();
+                    if (window.localStorage) {
+                        localStorage.setItem('yii2-debug-toolbar', 'maximized');
+                    }
+                });
+            });
+        })(jQuery);
+        jQuery('[data-toggle=popover]').popover();
+        jQuery('[data-toggle=tooltip]').tooltip();
+        jQuery('#yw0').yiiListView({
+            'ajaxUpdate': ['yw0'],
+            'ajaxVar': 'ajax',
+            'pagerClass': 'pagination',
+            'loadingClass': 'list\x2Dview\x2Dloading',
+            'sorterClass': 'sorter',
+            'enableHistory': false,
+            'afterAjaxUpdate': function() {
+                jQuery('.popover').remove();
+                jQuery('[data-toggle=popover]').popover();
+                jQuery('.tooltip').remove();
+                jQuery('[data-toggle=tooltip]').tooltip();
+            }
+        });
+        jQuery('#yw8_0 .alert').alert();
+    });
+    /*]]>*/
+    </script>
+</body>
+
+</html>
